ce9d9140a3dbc380bbb4eae10d92edb8997de5ff
Yakhis <watermelon22303@gmail.com>
2025-06-02 19:36:17 +0300

    Приближение к e

---
+++ Prigl_e/Prigl_e/Prigl_e.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Prigl_e
+{
+    static void Main()
+    {
+        Console.Write("Введите значение x (-1 <= x <= 1): ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        double slag = 1;
+        double sum = 1;
+        int n = 1;
+
+        while (Math.Abs(slag) >= 1e-6)
+        {
+            slag *= x / n;
+            sum += slag;
+            n++;
+        }
+
+        Console.WriteLine($"Приближенное значение e^{x} = {sum}");
+    }
+}
