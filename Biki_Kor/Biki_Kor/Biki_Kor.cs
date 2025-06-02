070f35b8124d308b5360ebac9495e2ff797b3279
Yakhis <watermelon22303@gmail.com>
2025-06-02 19:29:17 +0300

    Быки коровы

---
+++ Biki_Kor/Biki_Kor/Biki_Kor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Biki_Kor
+{
+    static void Main()
+    {
+        Console.WriteLine("Игра: Быки и Коровы");
+
+        Console.WriteLine("Введите четырехзначное число для начала игры : ");
+        string Numb = Console.ReadLine();
+        int popitok = 0;
+
+        while (true)
+        {
+            Console.Write("Ваше предположение : ");
+            string guess = Console.ReadLine();
+
+            popitok++;
+
+            int Bik = CountBik(Numb, guess);
+            int Kor = CountKor(Numb, guess) - Bik;
+
+            Console.WriteLine($"{Bik} бык(а,ов), {Kor} коров(а,ы)");
+
+            if (Bik == 4)
+            {
+                Console.WriteLine($"Число угадано {Numb} за {popitok} попытку(ки,ок).");
+                break;
+            }
+        }
+    }
+
+    static int CountBik(string secret, string guess)
+    {
+        int Bik = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            if (secret[i] == guess[i])
+                Bik++;
+        }
+        return Bik;
+    }
+
+    static int CountKor(string secret, string guess)
+    {
+        int Kor = 0;
+        foreach (char c in guess)
+        {
+            if (secret.Contains(c))
+                Kor++;
+        }
+        return Kor;
+    }
+}
