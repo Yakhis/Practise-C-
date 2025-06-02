b69d9e9115824da900665a80c9c7f09458a6ac9f
Yakhis <watermelon22303@gmail.com>
2025-06-02 19:31:54 +0300

    Рисование окружности

---
+++ Krug/Krug/Krug.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Krug
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите радиус окружности: ");
+        int rad = Convert.ToInt32(Console.ReadLine()); 
+        double tol = 0.5;
+
+        for (int y = rad; y >= -rad; y--)
+        {
+            for (int x = -rad; x <= rad; x++)
+            {
+                double promej = Math.Sqrt(x * x + y * y);
+
+                if (promej >= rad - tol && promej <= rad + tol)
+                    Console.Write("*");
+                else
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
